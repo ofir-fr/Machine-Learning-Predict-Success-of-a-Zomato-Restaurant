c0418a17f9901736ef7ddd8317962a543c6ede9f
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-06-23 22:11:36 +0300

    Update Main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -11,9 +11,29 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 
 from DF_PreProcessing import *
+from BasicDataPulling import *
 
-zomatoDF = pd.read_csv('zomato.csv')
+zomatoDF = pd.read_csv('zomato.csv')                 # Database Import
 
-zomatoDF = zomataDFPreProcessing(zomatoDF)
+zomatoDF = zomataDFPreProcessing(zomatoDF)           # Data Pre-processing
 
 print("Data Pre-processing is Done...")
+
+### Creat a new smaller DF with only votes, cost & rating for data analysis
+ratingVotesCost = zomatoDF.groupby('name').agg({'votes':'sum', 'url':'count','approx_cost(for two people)':'mean','rate':'mean'})
+ratingVotesCost.columns = ['total_votes','total_unities','ang_approx_cost','mean_rating']
+ratingVotesCost['votes_per_unity'] = ratingVotesCost['total_votes']/ratingVotesCost['total_unities']
+popularity = ratingVotesCost.sort_values(by='total_unities', ascending=False)
+
+
+topVotes, leastVote = topFive(popularity)
+
+sn.set()
+
+fig, axes = plt.subplots(1,2,figsize=(10,5),sharey=True)
+
+axes[0].set_title('Top 5 most voted')
+sn.barplot(ax=axes[0],x=topVotes['total_votes'],y=topVotes.index)
+
+axes[1].set_title('Top 5 least voted')
+sn.barplot(ax=axes[1],x=leastVote['total_votes'],y=topVotes.index)
