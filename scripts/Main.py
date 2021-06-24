7a3e4ab5973ceee520a03b78f6552e270928ecfc
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-06-24 21:52:52 +0300

    Update Main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -21,12 +21,13 @@ print("Data Pre-processing is Done...")
 
 ### Creat a new smaller DF with only votes, cost & rating for data analysis
 ratingVotesCost = zomatoDF.groupby('name').agg({'votes':'sum', 'url':'count','approx_cost(for two people)':'mean','rate':'mean'})
-ratingVotesCost.columns = ['total_votes','total_unities','ang_approx_cost','mean_rating']
+ratingVotesCost.columns = ['total_votes','total_unities','avg_approx_cost','mean_rating']
 ratingVotesCost['votes_per_unity'] = ratingVotesCost['total_votes']/ratingVotesCost['total_unities']
 popularity = ratingVotesCost.sort_values(by='total_unities', ascending=False)
 
 
-topVotes, leastVote = topFive(popularity)
+# Extract and present top&least 5 restaurants by quantity of votes
+topVotes, leastVote = topFive(popularity, 'total_votes')
 
 sn.set()
 
@@ -37,3 +38,18 @@ sn.barplot(ax=axes[0],x=topVotes['total_votes'],y=topVotes.index)
 
 axes[1].set_title('Top 5 least voted')
 sn.barplot(ax=axes[1],x=leastVote['total_votes'],y=topVotes.index)
+
+
+# Extract and present top&least 5 restaurants by mMeal price
+topPrice, leastPrice = topFive(popularity, 'avg_approx_cost')
+
+sn.set()
+
+fig, axes = plt.subplots(1,2,figsize=(10,5),sharey=True)
+
+axes[0].set_title('Top 5 expensive')
+sn.barplot(ax=axes[0],x=topPrice['avg_approx_cost'],y=topPrice.index)
+
+axes[1].set_title('Top 5 least expensive')
+sn.barplot(ax=axes[1],x=leastPrice['avg_approx_cost'],y=leastPrice.index)
+
