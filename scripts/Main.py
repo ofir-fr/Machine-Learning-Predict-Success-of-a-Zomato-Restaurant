97d3d5f2e65f73163831bf929fdf564391a80a05
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-06-28 08:27:16 +0300

    Update Main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -53,3 +53,35 @@ sn.barplot(ax=axes[0],x=topPrice['avg_approx_cost'],y=topPrice.index)
 axes[1].set_title('Top 5 least expensive')
 sn.barplot(ax=axes[1],x=leastPrice['avg_approx_cost'],y=leastPrice.index)
 
+
+# How many restaurants offer table booking?
+axes[0].set_title('Online booking availability')
+axes[0] = plt.subplot2grid((1,2),(0,0))
+plt.pie(zomatoDF['book_table'].value_counts(), labels = ['cannot book', 'can book'], autopct='%1.0f%%')  # Pie chart showing table booking availability of the restaurants
+
+
+### How many restaurants offer online orders?
+axes[1].set_title('Online ordering availability')
+axes[1] = plt.subplot2grid((1,2),(0,1))
+plt.pie(zomatoDF['online_order'].value_counts(), labels = ['accept orders', 'do not accept orders'], autopct='%1.0f%%')  # Pie chart showing online oders availability of the restaurants
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
