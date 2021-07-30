e51e602dd1be4220b21fcfee64b9a353b76fd506
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-07-30 12:12:12 +0300

    Update BasicDataPulling.py

--- scripts/BasicDataPulling.py
+++ scripts/BasicDataPulling.py
@@ -10,12 +10,14 @@ Basic Data Pulling
 
 
 
-### Top 5 most and least restaurantes of any feature in the dataframe
+### Top 5 most votes restaurantes
+### Top 5 least voted restaurantes
 
 def topFive(popularity, Feature):
     
     topValues = popularity.sort_values(by=Feature, ascending=False).head(5)
     leastValues = popularity.sort_values(by=Feature, ascending=False).query(f'{Feature} > 0').tail(5)
+    
     return topValues, leastValues
 
 
@@ -30,7 +32,6 @@ def restaurantsFiler(restaurantsDF, maxPrice, location, votesRate, foodType):
 
 ### Translate adresses to geolocations
 ### Returns a new data frame with name and geolocation [lat and lon]
-
 
 def restaurantesGeolocation (restaurantsDF):
     
@@ -53,10 +54,28 @@ def restaurantesGeolocation (restaurantsDF):
     restaurantsGeolocation['latitude'] = lat
     restaurantsGeolocation['longitude'] = lon
     
-    Return restaurantsGeolocation
+    return restaurantsGeolocation
+
+
+### Returns a WordCloud object of top dishes by users based on restaurant type
+
+def retrieveWordCloud(zomatoDF, RestaurantType):
     
+    if RestaurantType == 'all':
+        restaurantWordsDF = zomatoDF                                          # use the entire DF
+    elif zomatoDF['rest_type'].str.contains(RestaurantType).any():
+        restaurantWordsDF = zomatoDF[zomatoDF['rest_type'] == RestaurantType] # unique Restaurant Type filtering of the DF
+    else:
+        return None                                          # restaurant type waf not found
     
-
-
-
-
+    dishes = ''                                              # list to individual dishes 
+    
+    for word in restaurantWordsDF['dish_liked'].dropna():    # scans all words for dishes   ; if the cell is empty, drop it
+        wordsList = word.split()                             # split the words in the cells
+        for i in range((len(wordsList))):
+           wordsList[i] = wordsList[i].lower()               # reduce all letter to low case to avoide duplicates
+        dishes = dishes +' '.join(wordsList)+' '             # merge words list
+        
+    stopwordsList = set(STOPWORDS)                           # list of stop words that we dont want to show in the word cloud
+    return WordCloud(stopwords = stopwordsList, width = 1500, height = 1500).generate(dishes)        #generate and return the word cloud
+    
