7e6641101f424dcdc0109ef5f011a845a5489bd6
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-07-30 18:57:56 +0300

    Update DF_PreProcessing.py

--- scripts/DF_PreProcessing.py
+++ scripts/DF_PreProcessing.py
@@ -1,5 +1,5 @@
 """""""""""
-Zomata Project: PreProcessing
+Zomata Project: PreProcessing 
 
 """""""""""
 
@@ -7,35 +7,43 @@ def zomataDFPreProcessing(zomatoDF):
 
     import numpy as np
     import pandas as pd
-    
+    import re                           #  provides regular expression matching operations
+
     
     zomatoDF = pd.read_csv('zomato.csv')
     
     
     ### Creats a list of the features
+    
     zomatoFeatureList= []
     for feature in zomatoDF.columns:
         if zomatoDF[feature].isnull().sum()>1:
             zomatoFeatureList.append(feature)
+    
     
     ### Initial statues of the data
     #for feature in zomatoFeatureList:
     #    print("{} had {} % missing values".format(feature,np.round(zomatoDF[feature].isnull().sum()/len(zomatoDF)*100,2)))
     ###
     
+    
     ### Transforms 'approx_cost(for two people)' from string to float by removing non-numeric values (',')
+    
     zomatoDF["approx_cost(for two people)"] = zomatoDF["approx_cost(for two people)"].fillna("0")                                 # fills empty samples with the value 0
     zomatoDF["approx_cost(for two people)"]=zomatoDF["approx_cost(for two people)"].astype(str).apply(lambda x:x.replace(',','')) # remove ','
     zomatoDF["approx_cost(for two people)"]=zomatoDF["approx_cost(for two people)"].astype(float)                                 # convert to float
     
     
     ### Transform all non-uniform data in 'Rate' to '0/5'
+    
     zomatoDF["rate"] = zomatoDF["rate"].fillna("0")                          # fills empty samples with the score 0
     zomatoDF["rate"] = zomatoDF["rate"].replace("NEW","0")                   # Converts new restaurant to grade 0
     zomatoDF["rate"] = zomatoDF["rate"].replace("-","0")                     # Converts the value '-' to 0
     zomatoDF["rate"] = zomatoDF["rate"].str.replace(" ","")                  # Remove redundant spaces
     
-    ###Transing all 'Rate's to a value in the range of 0 to 100 by deviding the score in 5 and multiplying the result by 100
+    
+    ### Transing all 'Rate's to a value in the range of 0 to 100 by deviding the score in 5 and multiplying the result by 100
+    
     SplitedRate= []
     for i in range(0,len(zomatoDF)):                                                                    # scanning the entire DF
         if zomatoDF["rate"][i] != "0":                                                                  # if sample value is not 0
@@ -46,6 +54,28 @@ def zomataDFPreProcessing(zomatoDF):
             
     zomatoDF["rate"].update(SplitedRate)                                    # update 'rate' with the new values
     zomatoDF["rate"] = zomatoDF["rate"].astype(float)                       # convert the array to float
+
+
+    ### Removal of irrelevant text from the reviews
+
+    cleanReviews= []
+    for i in range(0,len(zomatoDF)):                                         # scanning the entire DF
+        review = ''                                                     
+        if zomatoDF['reviews_list'][i] != None: 
+            review = zomatoDF['reviews_list'][i]                             # Retrive text from the cell and clean it
+            review = review.lower()
+            review = re.sub('[^a-zA-z]', ' ', review)
+            review = re.sub('rated', ' ',review)
+            review = re.sub('x', ' ',review)
+            review = re.sub(' +', ' ',review)
+            
+            cleanReviews.append(str(review))                                # Append clean text into a new cell of process array of strings
+            
+        else:
+            cleanReviews.append(None) 
+            
+    zomatoDF['reviews_list'].update(cleanReviews)                           # update 'reviews_list' with the new values
+
 
 
     return zomatoDF
