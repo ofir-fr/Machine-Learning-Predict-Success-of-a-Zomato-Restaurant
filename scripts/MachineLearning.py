31965994dc475473e31a0fc59da6fdfd3a16098f
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-08-12 08:14:47 +0300

    Update MachineLearning.py

--- scripts/MachineLearning.py
+++ scripts/MachineLearning.py
@@ -7,13 +7,17 @@ Zomata Project: Machine Learning
 # new restaurant - 0 rate
 # old restaurants with rating different then 0 for the trainning models 
 
-def initiatorMachineLearning(zomatoDF):
+def initiatorMachineLearning(zomatoDF, thresholdRating):
 
     newRestauransDF = zomatoDF[zomatoDF['rated']==0]
 
     trainTestRestaurantsDF = zomatoDF[zomatoDF['rated']==1]
     
     
+    ### Creation of target:
+    ### rating > thresholdRating for good reateurants and rating <= thresholdRating equals bad restaurants
+    
+   trainTestRestaurantsDF['target'] = trainTestRestaurantsDF.apply(lambda x:i if x > threshold else 0)
     
     
     
