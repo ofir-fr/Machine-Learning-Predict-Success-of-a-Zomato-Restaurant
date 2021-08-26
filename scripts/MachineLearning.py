7bbf64f4b1efef122b94000ceae9a5e24a1aef0f
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-08-26 08:01:14 +0300

    Update MachineLearning.py

--- scripts/MachineLearning.py
+++ scripts/MachineLearning.py
@@ -17,13 +17,16 @@ def initiatorMachineLearning(zomatoDF, thresholdRating):
     ### Creation of target:
     ### rating > thresholdRating for good reateurants and rating <= thresholdRating equals bad restaurants
 
-    trainTestRestaurantsDF['target'] = trainTestRestaurantsDF.apply(lambda x:1 if x > threshold else 0)
+   trainTestRestaurantsDF['target'] = trainTestRestaurantsDF.apply(lambda x:1 if x > threshold else 0)
 
-    # New feature: total_cuisines - the amount of meal types in each restaurant
-    trainTestRestaurantsDF['total_cuisines'] = trainTestRestaurantsDF['cuisines'].astype(str).apply(lambda x: len(x.split(',')))
+   # New feature: total_cuisines - the amount of meal dishes in each restaurant
+   trainTestRestaurantsDF['total_cuisines'] = trainTestRestaurantsDF['cuisines'].astype(str).apply(lambda x: len(x.split(',')))
+      
+   # New feature: multiple_rest_type - the amount of meal types in each restaurant
+   trainTestRestaurantsDF['multiple_rest_type'] = trainTestRestaurantsDF['rest_type'].astype(str).apply(lambda x: len(x.split(',')))
     
     
-    return trainTestRestaurantsDF
+   return trainTestRestaurantsDF
     
     
     
