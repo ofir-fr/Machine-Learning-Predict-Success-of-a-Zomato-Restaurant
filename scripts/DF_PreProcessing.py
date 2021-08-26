c796f10cb118aa0b4d28d8ff5314d53bbb369cca
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-08-26 13:02:25 +0300

    Update DF_PreProcessing.py

--- scripts/DF_PreProcessing.py
+++ scripts/DF_PreProcessing.py
@@ -98,6 +98,7 @@ def zomataDFPreProcessing(zomatoDF):
 # split the data to two categories: 
 # new restaurant - 0 rate
 # old restaurants with rating different then 0 for the trainning models 
+###
 def zomataDFReorganizing(zomatoDF, thresholdRating):
 
     newRestauransDF = zomatoDF[zomatoDF['rated']==0]
