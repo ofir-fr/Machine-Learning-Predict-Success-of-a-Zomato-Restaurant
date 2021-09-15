5fba12941419595c99157e679180059d05409940
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-09-15 18:41:21 +0300

    Update MachineLearning.py

--- scripts/MachineLearning.py
+++ scripts/MachineLearning.py
@@ -3,26 +3,16 @@ Zomata Project: Machine Learning
 
 """""""""""
 
-### Random forest function receives: DF, test size and random state
-### Function returns predictions arrays, the confusion matrix of the test and its accuracy score
+### Function returns x_train, x_test, y_train, y_test by users test size and random state parameters
 
-def applyRandomForest(finalZomatoDF, testSize, randomState):
+  
+def prepareTrainTestSplit(finalZomatoDF, testSize, randomState):
 
     x = finalZomatoDF.drop('target', axis=1)
     y = finalZomatoDF['target']
-    
-    
-    
+      
     x_train, x_test, y_train, y_test =  train_test_split(x, y, test_size = testSize, random_state = randomState47)
     
-    model = RandomForestClassifier()
-    
-    model.fit(x_train, y_train)
-    
-    predictions = model.predict(x_test)
-    
-   return predictions, confusion_matrix(predictions, y_test), accuracy_score(predictions, y_test)
-    
+   return x_train, x_test, y_train, y_test
 
-def apply()
 
