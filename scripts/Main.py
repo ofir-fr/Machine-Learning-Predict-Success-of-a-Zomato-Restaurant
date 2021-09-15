bcfe96a75a6d97e70831fd4cd729fdd9e8fdd909
ofir-frd <85901822+ofir-frd@users.noreply.github.com>
2021-09-15 17:33:50 +0300

    Update Main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -18,15 +18,21 @@ import folium
 from folium.plugins import HeatMap
 #!pip install wordcloud
 from wordcloud import WordCloud, STOPWORDS
+#!pip install xgboost
+from xgboost import XGBClassifier
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 from DF_PreProcessing import *
 from BasicDataPulling import *
 from MachineLearning import *
+
 
 def main():
     
